c4b78e172bbadcace3b1e8976864faac6763cbde
Volodymyr <b.lagumup.marafet@gmail.com>
2025-03-07 22:28:54 +0200

    fix json format

--- services/open_ai_service.py
+++ services/open_ai_service.py
@@ -42,6 +42,7 @@ def get_system_prompt(categories: list[str]):
         **Always return JSON output** in the format:
         
         ```json
+        "documents":
         [
             {{
                 "file_name": "File Name",
