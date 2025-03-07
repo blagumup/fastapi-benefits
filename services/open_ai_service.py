6b59923c592dec9f1dbe4ece5cf788fe64b5b13a
Volodymyr <b.lagumup.marafet@gmail.com>
2025-03-07 15:23:59 +0200

    prompt eng

--- services/open_ai_service.py
+++ services/open_ai_service.py
@@ -1,13 +1,15 @@
 import openai
 from config import get_settings
+from models.benefit_category import BenefitCategory
 
 settings = get_settings()
+benefit_category_values = [category.value for category in BenefitCategory]
 
-def parse_with_gpt(text):
+def parse_with_gpt(ocr_result, email_data):
     client = openai.OpenAI(api_key=settings.OPENAI_API_KEY)
 
-    system_prompt = get_system_prompt()
-    user_prompt = get_user_prompt()
+    system_prompt = get_system_prompt(benefit_category_values)
+    user_prompt = get_user_prompt(ocr_result, email_data)
 
     response = client.chat.completions.create(
         model=settings.OPENAI_MODEL,
@@ -34,21 +36,35 @@ def get_system_prompt(categories: list[str]):
         - Determine the **category** of the benefit letter from a predefined list:
         {categories}
 
-        - Extract structured fields based on the detected category.
+        - Extract structured fields based on the detected category for each document.
         **Always return JSON output** in the format:
         
         ```json
         {
+            "file_name": "File Name"
             "category": "Category Name",
-            "fields": {
             "username": "Extracted Name",
-            "email": "Extracted Email (if available)",
-            "amount": "Extracted Amount (if available)",
-            "date": "Date of the transaction",
-            "transaction_id": "Extracted Transaction ID (if available)",
-            "description": "Short description of the benefit claim"
-            }
+            "document_number": "Extracted Document Number"
+            "document_date": "Extracted Document Date",
+            "address": "Extracted Amount (if available)",
+            "document_sum": "Extracted document sum"
+            "document_currency": "Extracted document currency",
+            "account_number": "Extracted account number",
+            "additional_info": "Short description of the benefit claim"
+        },
+        {
+            "file_name": "File Name"
+            "category": "Category Name",
+            "username": "Extracted Name",
+            "document_number": "Extracted Document Number"
+            "document_date": "Extracted Document Date",
+            "address": "Extracted Amount (if available)",
+            "document_sum": "Extracted document sum"
+            "document_currency": "Extracted document currency",
+            "account_number": "Extracted account number",
+            "additional_info": "Short description of the benefit claim"
         }
+
         ```
         
         - **If any field is missing in the document, return `null` for that field**.  
