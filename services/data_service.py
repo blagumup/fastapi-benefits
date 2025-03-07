97734ebc2d548edc712e615fedff3939dead8af4
Volodymyr <b.lagumup.marafet@gmail.com>
2025-03-07 18:53:41 +0200

    add save_compensation_request

--- services/data_service.py
+++ services/data_service.py
@@ -1,3 +1,5 @@
+from uuid import uuid4
+from services.utils import get_user_id_by_email
 from repositories.benefit_repository import BenefitRepository
 
 def save_benefit_request():
@@ -11,3 +13,13 @@ def get_compensation_request():
 
 def get_benefit_categories():
     return
+
+def save_compensation_request(parsed_data, email_data):
+    request_id = uuid4()
+    employee_id = get_user_id_by_email()
+    email_subject = email_data['subject']
+    email_body = email_data['body']
+
+    BenefitRepository.save_compensation_request(request_id, employee_id, email_subject, email_body)
+
+    return request_id
