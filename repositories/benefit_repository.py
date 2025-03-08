8cc0d51202c72eb6981ef296cb814f71d1a695dc
Volodymyr <b.lagumup.marafet@gmail.com>
2025-03-08 15:28:49 +0200

    delete unused code

--- repositories/benefit_repository.py
+++ repositories/benefit_repository.py
@@ -3,16 +3,6 @@ from repositories.database import db
 
 class BenefitRepository:
     @staticmethod
-    def save_benefit(username, email, amount, date, transaction_id, category, description):
-        """Insert a new benefit request into the database."""
-        query = """
-        INSERT INTO benefits (username, email, amount, date, transaction_id, category, description)
-        VALUES (%s, %s, %s, %s, %s, %s, %s)
-        RETURNING id;
-        """
-        db.execute(query, username, email, amount, date, transaction_id, category, description)
-
-    @staticmethod
     def get_all_benefits():
         """Retrieve all benefits."""
         result = db.fetch('fn_benefit_categories_get_all')
@@ -37,6 +27,5 @@ class BenefitRepository:
     @staticmethod
     def get_benefit_by_id(benefit_id):
         """Retrieve a benefit by ID."""
-        # query = "SELECT * FROM benefits WHERE id = %s;"
         return db.fetch("procedure_name", benefit_id)
     
--- services/data_service.py
+++ services/data_service.py
@@ -7,8 +7,6 @@ from repositories.benefit_repository import BenefitRepository
 from repositories.employees_repository import EmployeeRepository
 from repositories.compensation_repository import CompensationRepository
 
-def save_benefit_request():
-    return
 
 def get_benefit_report(report_id: int):
     return BenefitRepository.get_benefit_by_id(report_id)
