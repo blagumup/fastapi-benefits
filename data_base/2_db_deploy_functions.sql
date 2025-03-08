701415a256c734fee942e02ceb8be156d0cf3560
Mykola Pavshuk <mpavshuk>
2025-03-08 15:30:55 +0100

    updated get requests fn

--- data_base/2_db_deploy_functions.sql
+++ data_base/2_db_deploy_functions.sql
@@ -327,38 +327,40 @@ DECLARE
     v_request_id UUID;
 BEGIN
 
-	v_request_id := (SELECT request_id FROM compensation_request_records WHERE record_id = p_record_id);
+    v_request_id := (SELECT request_id FROM compensation_request_records WHERE record_id = p_record_id);
 
-	UPDATE compensation_request_records
-		SET status_id = p_status_id,
-			updated_date = date_current
-	WHERE record_id = p_record_id;
-	
-	WITH record_statuses AS (
-		SELECT 
-			request_id,
-			ARRAY_AGG(DISTINCT status_id) AS status_list
-		FROM compensation_request_records
-		WHERE request_id = v_request_id
-		GROUP BY request_id
-	),
-	status_updates AS (
-		SELECT 
-			request_id,
-			CASE 
-				WHEN ARRAY['open', 'exceeded_limit'] && status_list THEN 'open'
-				WHEN 'waiting_for_clarification' = ANY(status_list) THEN 'waiting_for_clarification'
-				WHEN status_list <@ ARRAY['approved', 'declined', 'cancelled'] THEN 'processed'
-			END AS new_status
-		FROM record_statuses
-	)
-	UPDATE employee_compensation_request ecr
-		SET status_id = cs.status_id
-	FROM status_updates su
-	JOIN compensation_status cs 
-		ON su.new_status = cs.status_name
-	WHERE ecr.request_id = su.request_id
-	;
+    UPDATE compensation_request_records
+    SET status_id = p_status_id,
+        updated_date = date_current
+    WHERE record_id = p_record_id;
+
+    WITH record_statuses AS (
+        SELECT 
+            cr.request_id,
+            ARRAY_AGG(DISTINCT cs.status_name) AS status_list
+        FROM compensation_request_records cr
+        JOIN compensation_status cs ON cr.status_id = cs.status_id
+        WHERE cr.request_id = v_request_id
+        GROUP BY cr.request_id
+    ),
+    status_updates AS (
+        SELECT 
+            request_id,
+            CASE 
+                WHEN ARRAY['open', 'exceeded_limit'] && status_list THEN 'open'
+                WHEN 'waiting_for_clarification' = ANY(status_list) THEN 'waiting_for_clarification'
+                WHEN status_list <@ ARRAY['approved', 'declined', 'cancelled'] THEN 'processed'
+                ELSE NULL 
+            END AS new_status
+        FROM record_statuses
+    )
+    UPDATE employee_compensation_request ecr
+    SET status_id = cs.status_id
+    FROM status_updates su
+    JOIN compensation_status cs 
+        ON su.new_status = cs.status_name
+    WHERE ecr.request_id = su.request_id
+    AND su.new_status IS NOT NULL; 
 
 END;
 $$ LANGUAGE plpgsql;
